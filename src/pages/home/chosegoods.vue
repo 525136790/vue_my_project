020343061964f90e51a37434c8265c8d8a1c0fe9
baixuejing <525136790@qq.com>
2020-03-22 15:27:53 +0800

    Update chosegoods.vue

--- src/pages/home/chosegoods.vue
+++ src/pages/home/chosegoods.vue
@@ -19,7 +19,9 @@ export default {
     name:'chosegoods',
     data(){
         return {
-            choseArr:[]
+            choseArr:[{
+                productDetail:[]
+            }]
         }
     },
     created(){
